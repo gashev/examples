13851897c8a70828810e1d99d834545534c637c3
Oleg Gashev <oleg@gashev.net>
2016-04-16 13:35:29 +0000

    Code cleanup: removed redundant code.

--- libsolv/save_repo_to_solv_file.py
+++ libsolv/save_repo_to_solv_file.py
@@ -24,8 +24,8 @@ def download_archive(file, url):
 
 def find(handle, what):
     di = handle.Dataiterator_meta(
-        solv.REPOSITORY_REPOMD_TYPE, 
-        what, 
+        solv.REPOSITORY_REPOMD_TYPE,
+        what,
         solv.Dataiterator.SEARCH_STRING
     )
     di.prepend_keyname(solv.REPOSITORY_REPOMD)
@@ -34,27 +34,6 @@ def find(handle, what):
         return dp.lookup_str(solv.REPOSITORY_REPOMD_LOCATION)
     return None
 
-def add_ext_keys(ext, repodata, handle):
-    if ext == 'DL':
-        repodata.add_idarray(handle, solv.REPOSITORY_KEYS, solv.REPOSITORY_DELTAINFO)
-        repodata.add_idarray(handle, solv.REPOSITORY_KEYS, solv.REPOKEY_TYPE_FLEXARRAY)
-    if ext == 'FL':
-        repodata.add_idarray(handle, solv.REPOSITORY_KEYS, solv.SOLVABLE_FILELIST)
-        repodata.add_idarray(handle, solv.REPOSITORY_KEYS, solv.REPOKEY_TYPE_DIRSTRARRAY)
-   
-def add_ext(handle, repodata, what, ext):
-    filename = find(handle, what)
-    if not filename and what == 'deltainfo':
-        filename = find(handle, 'prestodelta')
-    if not filename:
-        return
-
-    new_handle = repodata.new_handle()
-    repodata.set_poolstr(new_handle, solv.REPOSITORY_REPOMD_TYPE, what)
-    repodata.set_str(new_handle, solv.REPOSITORY_REPOMD_LOCATION, filename)
-    add_ext_keys(ext, repodata, new_handle)
-    repodata.add_flexarray(solv.SOLVID_META, solv.REPOSITORY_EXTERNAL, new_handle)
- 
 pool = solv.Pool()
 pool.setarch()
 
@@ -71,14 +50,6 @@ updateinfo_file_name = find(handle, 'updateinfo')
 updateinfo_url = base_url + updateinfo_file_name
 updateinfo_f = download_archive(updateinfo_file_name, updateinfo_url)
 handle.add_updateinfoxml(updateinfo_f, 0)
-
-repodata = handle.add_repodata(0)
-add_ext(handle, repodata, 'deltainfo', 'DL')
-add_ext(handle, repodata, 'filelists', 'FL')
-repodata.internalize()
-
-handle.create_stubs()
-pool.createwhatprovides()
 
 f_solv = open('/tmp/solv' , 'wb')
 f_solv = solv.xfopen_fd(None, f_solv.fileno())
